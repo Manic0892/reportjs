a631110020ee302da865774295462ef0b50d46fe
Sean McGeer <basbalmanic@gmail.com>
2015-03-01 10:00:00 -0800

    main.js

--- routes/main.js
+++ routes/main.js
@@ -2,19 +2,19 @@
 // All "miscellaneous" or "main" routes that are not devoted to a specific function.  Currently just includes handling of /, aka the homepage.
 
 module.exports = function(app,db) {
-	
+
 	app.get(base + '/', function(req,res) {
-		res.send('Report JS');
+		res.send('ReportJS');
 	});
-	
+
 	app.get(base + '/report', function(req,res) {
 		res.send('Report an issue.');
 	});
-	
+
 	app.post(base + '/report', function(req,res) {
 		res.send('Report an issue using a prefilled form.');
 	});
-	
+
 	app.post(base + '/report/submit', function(req,res) {
 		res.send('Finalizing submitted issue.');
 	});
